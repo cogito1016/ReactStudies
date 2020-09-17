4c3ba7e8981bac3837b9b77f20682feaca9a46b9
Jayden <cogito1016@fadutec.com>
2020-09-17 16:27:54 +0900

    Webpack & babel의 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //리액트 모듈을 불러옴
+import logo from './logo.svg'; //로더(웹팩의 기능) file-loader의 역할 
+import './App.css';  //css-loader의 역할
+
+//babel-loader는 최신 자바스크립트로 작성된 코드를 ES5문법으로 변환해줌
+//==> 브라우저 버전마다의 호환성과 JSX문법의 변환을 위해 필요
 
 function App() {
   return (
